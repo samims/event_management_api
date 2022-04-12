2d9db18ec09e146a86f72f2ffbb06b823ba9779a
Samiul Sk <samiul@rely.sg>
2022-04-12 17:37:49 +0530

    add event create api

--- events/serializers.py
+++ events/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
 from .models import Event
@@ -17,8 +18,31 @@ class EventListSerializer(serializers.ModelSerializer):
         """
         data = super(EventListSerializer, self).to_representation(instance)
         # we should show details of participants in public list api
+        # for admin it's ok
         if not self.context['request'].user.is_superuser:
             data.pop('participants')
         return data
 
 
+class EventCreateSerializer(serializers.ModelSerializer):
+    """
+    Serializer for Event Create View
+    """
+    is_active = serializers.BooleanField(default=True)
+
+    class Meta:
+        model = Event
+        fields = '__all__'
+
+    def validate(self, data):
+        """
+        Validate the data
+        """
+        # check if the user is admin
+        if not self.context['request'].user.is_superuser:
+            raise serializers.ValidationError(
+                'Only admin can create events')
+
+        # set the owner of the event to the current user
+        data['organizer'] = self.context['request'].user
+        return data
--- events/views.py
+++ events/views.py
@@ -1,11 +1,11 @@
-from rest_framework.generics import ListAPIView
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.generics import ListCreateAPIView
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser
 
 from .models import Event
-from events.serializers import EventListSerializer
+from events.serializers import EventListSerializer, EventCreateSerializer
 
 
-class EventListCreateAPIView(ListAPIView):
+class EventListCreateAPIView(ListCreateAPIView):
     """
     List all events, or create a new event.
     """
@@ -13,3 +13,27 @@ class EventListCreateAPIView(ListAPIView):
     queryset = Event.objects.order_by('-start_date')
     permission_classes = (IsAuthenticatedOrReadOnly,)
 
+    def get_serializer_class(self):
+        """
+        Override the default serializer class
+        to return the appropriate serializer class for request method.
+        """
+        if self.request.method == 'GET':
+            return EventListSerializer
+        return EventCreateSerializer
+
+    def get_permissions(self):
+        """
+        Override the default permission class
+        to allow only admin users to create events.
+        """
+        if self.request.method == 'GET':
+            return [IsAuthenticatedOrReadOnly()]
+        return [IsAdminUser()]
+
+    def perform_create(self, serializer):
+        """
+        Override the default create method to
+        set the user that created the event.
+        """
+        serializer.save(organizer=self.request.user)
