a38af0915f3ce09c1b9ba64ff063f986f3fa7993
Samiul Sk <samiul@rely.sg>
2022-04-12 22:35:42 +0530

    add model admin for booking

--- events/admin.py
+++ events/admin.py
@@ -3,5 +3,13 @@ from django.contrib import admin
 from .models import Event, Booking
 
 
+#
+class CustomBookingAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'event', 'participant',
+        'booking_code', 'created_at'
+    )
+
+
 admin.site.register(Event)
-admin.site.register(Booking)
+admin.site.register(Booking, CustomBookingAdmin)
