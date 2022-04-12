683a8e6f3bf7598c54ac50b5073abfbfb36afa9b
Samiul Sk <samiul@rely.sg>
2022-04-12 20:22:34 +0530

    add custom permission as it can be used

---
+++ core/permissions.py
@@ -0,0 +1,14 @@
+from rest_framework.permissions import BasePermission
+
+
+class IsAdminOrOwnerOnly(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        """
+        Defines if the user has permission to access the object.
+        Used for the detail view.
+        """
+        return (
+                request.user.is_authenticated and
+                request.user.is_staff or
+                obj.owner == request.user
+        )
