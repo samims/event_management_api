e7dc25c2401b72ac05f30c76f4b12d56e45b0929
Samiul Sk <samiul@rely.sg>
2022-04-13 12:47:35 +0530

    add validation to user email field uniqueness as default one isn't working

--- accounts/models.py
+++ accounts/models.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import AbstractUser
 
 
 # Keeping scope to modify the user model
+from django.core.validators import validate_email
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
@@ -12,5 +13,22 @@ class CustomUser(AbstractUser):
     in the future
     """
     # making email fields unique cause it makes sense :)
-    email = models.EmailField(_('email address'), blank=False, unique=True)
+    email = models.EmailField(_('email address'), blank=False, unique=True, validators=[validate_email])
+
+    def __str__(self):
+        return self.email
+
+    # validate the email field
+    def clean(self):
+        """
+        Validate the email field to make sure it is a valid email address.
+        for some weird reason, django does not validate the email field on creation
+        """
+        validate_email(self.email)
+        super(CustomUser, self).clean()
+
+    def save(self, *args, **kwargs):
+        self.clean()
+        return super(CustomUser, self).save(*args, **kwargs)
+
 
--- accounts/tests/test_custom_user_model.py
+++ accounts/tests/test_custom_user_model.py
@@ -1,3 +1,2 @@
+from .models import CustomUser
 from django.test import TestCase
-
-# Create your tests here.
